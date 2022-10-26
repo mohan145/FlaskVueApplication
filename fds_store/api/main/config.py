bab774f01aea262ea9cad799761edc3bb3935cdf
Mohan Krishna Kosetti <mohan145krishna@gmail.com>
2022-10-26 11:47:15 +0530

    Added Config file and

---
+++ fds_store/api/main/config.py
@@ -0,0 +1,49 @@
+db_config ={
+    'DEV':{
+        'DRIVER':"postgresql",
+        'USERNAME':"postgres",
+        'PASSWORD':"123Welcome",
+        'HOST':"192.168.1.6",
+        'PORT':'5432',
+        'DB_NAME':'postgres'
+    },
+'PROD':{
+        'DRIVER':"postgresql",
+        'USERNAME':"postgres",
+        'PASSWORD':"123Welcome",
+        'HOST':"192.168.1.6",
+        'PORT':'5432',
+        'DB_NAME':'postgres'
+    },
+'STG':{
+        'DRIVER':"postgresql",
+        'USERNAME':"postgres",
+        'PASSWORD':"123Welcome",
+        'HOST':"192.168.1.6",
+        'PORT':'5432',
+        'DB_NAME':'postgres'
+    }
+}
+
+class Configuration:
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    ENV = 'PROD'
+    SQLALCHEMY_DATABASE_URI = db_config[ENV]['DRIVER']+"://"+db_config[ENV]['USERNAME']+":"+db_config[ENV]['PASSWORD']+"@"+db_config[ENV]['HOST']+":"+db_config[ENV]['PORT']+"/"+db_config[ENV]['DB_NAME']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Configuration):
+    DEBUG = True
+    TESTING = True
+    ENV = 'DEV'
+
+class StagingConfig(Configuration):
+    DEBUG = False
+    TESTING = True
+    ENV = 'STG'
+
+class ProductionConfig(Configuration):
+    DEBUG  = False
+
+Config = dict(dev=DevelopmentConfig,stg=StagingConfig,prod=ProductionConfig)
